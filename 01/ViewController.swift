2b0763c7d73b452393ccfa56afda3bce6d2223cf
20171105112 <903056683@qq.com>
2018-09-27 09:38:22 +0800

    计算器按钮

--- 01/ViewController.swift
+++ 01/ViewController.swift
@@ -9,12 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var texe: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func class1(_ sender: Any) {
+    }
+    
+    @IBAction func class2(_ sender: Any) {
+    }
+    
+    @IBAction func class3(_ sender: Any) {
+    }
+    
+    @IBAction func class4(_ sender: Any) {
+    }
+    @IBAction func class5(_ sender: Any) {
+    }
+    @IBAction func class6(_ sender: Any) {
+    }
+    @IBAction func class7(_ sender: Any) {
+    }
+    
+    @IBAction func class8(_ sender: Any) {
+    }
+    
+    @IBAction func class9(_ sender: Any) {
+    }
+    
+    @IBAction func class0(_ sender: Any) {
+    }
+    @IBAction func classadd(_ sender: Any) {
+    }
+    @IBAction func classjian(_ sender: Any) {
+    }
+    @IBAction func classchen(_ sender: Any) {
+    }
+    @IBAction func classchu(_ sender: Any) {
+    }
+    @IBAction func classdeng(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
